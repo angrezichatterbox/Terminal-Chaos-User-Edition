4b51ce77162938a33193ada43548300ebdd4ffb7
amansxcalibur <notifyamanvs@gmail.com>
2024-03-27 02:01:42 +0530

    Added Dark book I script to Chest.py

--- Arrakis-dex/Eolian Caves/01D/.01/narrow path/path/0DI/02/01/Chamber/Chest.py
+++ Arrakis-dex/Eolian Caves/01D/.01/narrow path/path/0DI/02/01/Chamber/Chest.py
@@ -1,5 +1,10 @@
+def unlocked():
+        with open("DarkBookI.txt","w") as file:
+                file.write("a=0DdcDACkhhcXXXXXXX\n")
+
 print("The noxious fumes of corruption encapsulates the chest. Use artifact containing holy light magic to disperse it and uncover the secrets the treasurous shrine holds..")
 if "CSigVmaroAn"==input():
-	print("unlocked")
+	print("SHEOOOOOOOHHHH")
+	unlocked()
 else:
 	print("try again")
