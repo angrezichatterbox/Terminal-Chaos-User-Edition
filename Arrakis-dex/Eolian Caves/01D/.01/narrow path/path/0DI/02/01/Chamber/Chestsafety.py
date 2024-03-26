24b56598713b8b595e2b16fb3537ea22b6977210
amansxcalibur <notifyamanvs@gmail.com>
2024-03-27 01:47:07 +0530

    Dark realm I added with amulet use implemented via pyc but no script

---
+++ Arrakis-dex/Eolian Caves/01D/.01/narrow path/path/0DI/02/01/Chamber/Chestsafety.py
@@ -0,0 +1,5 @@
+print("The noxious fumes of corruption encapsulates the chest. Use artifact containing holy light magic to disperse it and uncover the secrets the treasurous shrine holds..")
+if "CSigVmaroAn"==input():
+	print("unlocked")
+else:
+	print("try again")
