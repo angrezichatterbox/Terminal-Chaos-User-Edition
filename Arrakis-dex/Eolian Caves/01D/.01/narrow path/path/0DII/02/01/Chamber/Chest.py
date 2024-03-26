4d897cfce3f325da393aea0d37829b19c6d61dae
amansxcalibur <notifyamanvs@gmail.com>
2024-03-27 02:14:53 +0530

    Added Dark branch II all the way till script for bookII creation

---
+++ Arrakis-dex/Eolian Caves/01D/.01/narrow path/path/0DII/02/01/Chamber/Chest.py
@@ -0,0 +1,10 @@
+def unlocked():
+        with open("DarkBookII.txt","w") as file:
+                file.write("a=XXXXXXXXXXXnw=0PHf\n")
+
+print("The noxious fumes of corruption encapsulates the chest. Use artifact containing holy light magic to disperse it and uncover the secrets the treasurous shrine holds..")
+if "CSigVmaroAn"==input():
+	print("SHEOOOOOOOHHHH")
+	unlocked()
+else:
+	print("try again")
